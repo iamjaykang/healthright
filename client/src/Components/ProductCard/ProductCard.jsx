24d4705fed813ee1af8ced0ec343d3477230d35b
Jay Kang <jaykang0395@gmail.com>
2023-01-05 00:17:37 +1300

    feat: added product card component and styles

---
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "../../App/Common/Button/Button";
+import "./ProductCard.css";
+
+const ProductCard = ({ product }) => {
+  const { name, price, imgUrl, id } = product;
+  return (
+    <div className="product-card-container">
+      <img src={imgUrl} alt={`${name}`} />
+      <div className="card-info">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <Button btnType="inverted">Add to Cart</Button>
+    </div>
+  );
+};
+
+export default ProductCard;
