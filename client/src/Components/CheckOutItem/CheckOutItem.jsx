0419d191230f8ad16942c323bdca29927aed39e8
Jay Kang <jaykang0395@gmail.com>
2023-01-06 02:50:27 +1300

    styles: style changes for checkout page

--- client/src/Components/CheckOutItem/CheckOutItem.jsx
+++ client/src/Components/CheckOutItem/CheckOutItem.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { CartContext } from "../../Contexts/Cart";
+import "./CheckOutItem.css";
 
-const CheckOutItem = () => {
+const CheckOutItem = ({ item }) => {
+  const { updateItemQuantity, removeItem } = useContext(CartContext);
+
+  const handleIncrementClick = (itemId) => {
+    updateItemQuantity(itemId, "increment");
+  };
+
+  const handleDecrementClick = (itemId) => {
+    updateItemQuantity(itemId, "decrement");
+  };
   return (
-    <div>CheckOutItem</div>
-  )
-}
+    <>
+      <div className="checkout-item-container">
+        <div className="img-container">
+          <img src={item.imgUrl} alt={`${item.name}`} />
+        </div>
+        <span className="name">{item.name}</span>
 
-export default CheckOutItem
+        <div className="quantity">
+          <div onClick={() => handleDecrementClick(item.id)}>-</div>
+          <span className="value">{item.quantity}</span>
+          <div onClick={() => handleIncrementClick(item.id)}>+</div>
+        </div>
+
+        <span className="price">{item.price}</span>
+
+        <div onClick={() => removeItem(item.id)} className="remove-btn">
+          X
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CheckOutItem;
--- client/src/Components/CheckOutList/CheckOutList.jsx
+++ client/src/Components/CheckOutList/CheckOutList.jsx
@@ -1,57 +1,34 @@
 import { useContext } from "react";
 import { CartContext } from "../../Contexts/Cart";
+import CheckOutItem from "../CheckOutItem/CheckOutItem";
+import './CheckOutList.css'
 
 const CheckOutList = () => {
-  const { cartItems, updateItemQuantity, removeItem, cartTotal } = useContext(CartContext);
-
-  const handleIncrementClick = (itemId) => {
-    updateItemQuantity(itemId, "increment");
-  };
-
-  const handleDecrementClick = (itemId) => {
-    updateItemQuantity(itemId, "decrement");
-  };
+  const { cartItems, cartTotal } = useContext(CartContext);
 
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Quantity</th>
-            <th>Price</th>
-            <th>Remove</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartItems &&
-            cartItems.map((item) => (
-              <tr key={item.id}>
-                <td>
-                  <img src={item.imgUrl} alt={`${item.name}`} />
-                </td>
-                <td>
-                  <span>{item.name}</span>
-                </td>
-                <td>
-                  <button onClick={() => handleDecrementClick(item.id)}>
-                    -
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => handleIncrementClick(item.id)}>
-                    +
-                  </button>
-                </td>
-                <td>{item.price}</td>
-                <td>
-                  <button onClick={() => removeItem(item.id)}>x</button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-      <div>Total: ${cartTotal}</div>
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+
+            <span>Product</span>
+        </div>
+        <div className="header-block">
+            <span>Name</span>
+        </div>
+        <div className="header-block">
+            <span>Quantity</span>
+        </div>
+        <div className="header-block">
+            <span>Price</span>
+        </div>
+        <div className="header-block">
+            <span>Remove</span>
+        </div>
+      </div>
+        {cartItems &&
+          cartItems.map((item) => <CheckOutItem key={item.id} item={item} />)}
+      <span className="total">Total: ${cartTotal}</span>
     </div>
   );
 };
