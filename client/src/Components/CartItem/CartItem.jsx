d4da205fb7b96ee3e431522fb32b0e824cad28b9
Jay Kang <jaykang0395@gmail.com>
2023-01-05 22:42:54 +1300

    feat: added cart icon increment and styles for cart drop down

--- client/src/Components/CartItem/CartItem.jsx
+++ client/src/Components/CartItem/CartItem.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import './CartItem.css';
 
 const CartItem = ({ cartItem }) => {
-  const { name, quantity } = cartItem;
+  const { name, quantity, price, imgUrl } = cartItem;
+  console.log(cartItem);
   return (
-    <div>
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+    <div className="cart-item-container">
+      <img src={imgUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">{quantity} x {price}</span>
+      </div>
     </div>
   );
 };
--- client/src/Contexts/Cart.jsx
+++ client/src/Contexts/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   // find if cartItems contains productToAadd
@@ -24,17 +24,33 @@ export const CartContext = createContext({
   addItemToCart: () => {},
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
