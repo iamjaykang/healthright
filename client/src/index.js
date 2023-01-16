7660d8ff19d783348b40c5fb8c4ab9f14e24f999
Jay Kang <jaykang0395@gmail.com>
2023-01-17 03:08:53 +1300

    Added stripe.utils.js and removed .env

--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,16 @@ import { router } from "./App/Routes/Routes";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./stores/store";
+import { Elements } from "@stripe/react-stripe-js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <RouterProvider router={router} />
+        <Elements>
+          <RouterProvider router={router} />
+        </Elements>
       </PersistGate>
     </Provider>
   </React.StrictMode>
