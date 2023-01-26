024f8cb8dbf2beaac20e1fcecc935fe7a29a03b4
Jay Kang <jaykang0395@gmail.com>
2023-01-26 15:45:24 +1300

    Updated styling for NavLink and DropDown

--- client/src/App/Layout/Header/Navbar/Navbar.layout.jsx
+++ client/src/App/Layout/Header/Navbar/Navbar.layout.jsx
@@ -3,9 +3,11 @@ import { NavLink } from "react-router-dom";
 import navLinks from "../../../../assets/data/navLinks.json";
 import CartIcon from "../../../../Components/CartIcon/CartIcon.component";
 import NavDropdown from "../../../../Components/NavDropdown/NavDropdown.component";
-import {useDispatch} from 'react-redux'
-import './Navbar.css'
+import { useDispatch } from "react-redux";
+import "./Navbar.css";
 import { signOutLoading } from "../../../../stores/user/user.action";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Navbar = ({ currentUser }) => {
   const [dropdownOpen, setDropdownOpen] = useState({});
@@ -14,29 +16,35 @@ const Navbar = ({ currentUser }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+  // if a nav item is being hovered over, set the corresponding dropdown to open
     if (hovered) {
       setDropdownOpen({ [hovered]: true });
     }
   }, [hovered]);
 
+  // function to handle mouse enter event on a nav item
   const handleMouseEnter = (id) => {
+    // clear any previous timeout to close the dropdown
     clearTimeout(timeoutId);
     setHovered(id);
   };
 
+  // function to handle mouse leave event on a nav item
   const handleMouseLeave = () => {
+    // set a timeout to close the dropdown after 300ms
     timeoutId = setTimeout(() => {
       setDropdownOpen({ [hovered]: false });
       setHovered(null);
     }, 300);
   };
 
-  const signOutUser = () => dispatch(signOutLoading())
+  const signOutUser = () => dispatch(signOutLoading());
 
   return (
     <nav className="navbar">
       <ul className="navbar-group">
         {navLinks.map((navLink) =>
+          // if the navLink title is "Sign In" and there is a current user, set navLink to "Sign Out"
           navLink.title === "Sign In" && currentUser ? (
             <li className="nav-item" key={navLink.id}>
               <NavLink className="nav-link" to="#" onClick={signOutUser}>
@@ -52,6 +60,17 @@ const Navbar = ({ currentUser }) => {
             >
               <NavLink className="nav-link" to={navLink.to}>
                 <span>{navLink.title}</span>
+                {/* if the navLink dropdown exists and is hovered over render expandMoreIcon else expandLessIcon */}
+                {navLink.dropdown &&
+                  (dropdownOpen[hovered] ? (
+                    <span>
+                      <ExpandMoreIcon />
+                    </span>
+                  ) : (
+                    <span>
+                      <ExpandLessIcon />
+                    </span>
+                  ))}
               </NavLink>
               {navLink.dropdown && (
                 <NavDropdown
@@ -66,7 +85,7 @@ const Navbar = ({ currentUser }) => {
           )
         )}
         <li className="nav-item">
-          <CartIcon isMobileMenu={false}/>
+          <CartIcon isMobileMenu={false} />
         </li>
       </ul>
     </nav>
