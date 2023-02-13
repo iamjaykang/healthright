0c37e93e4846accc6101210669326d0defa75c4e
Jay Kang <jaykang0395@gmail.com>
2023-02-14 02:09:38 +1300

    Added routes to implement admin routes for multiple admin components

--- client/src/app/routes/Routes.jsx
+++ client/src/app/routes/Routes.jsx
@@ -1,26 +1,46 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import Authentication from "../../components/authentication/AuthenticationPage.component";
+import AuthenticationPage from "../../components/authentication/AuthenticationPage.component";
 import Brands from "../../components/brands/BrandsPage.component";
 import App from "../layout/App";
 import Brand from "../../components/brand/BrandPage.component";
 import PaymentPage from "../../components/payment/PaymentPage.component";
 import NotFoundPage from "../../components/notFound/NotFoundPage.component";
 import CheckOutPage from "../../components/checkOut/CheckOutPage.component";
+import RequireAuth from "./RequireAuth";
+import AdminAuthenticationPage from "../../components/admin/authentication/AdminAuthentication.component";
+import AdminHome from "../../components/admin/dashboard/home/AdminHome.component";
+import AdminOrders from "../../components/admin/dashboard/orders/AdminOrders.components";
 
 export const routes = [
   {
     path: "/",
     element: <App />,
     children: [
+      {
+        path: "/admin",
+        element: <RequireAuth />,
+        children: [
+          {
+            path: "/admin/dashboard",
+            element: <AdminHome />,
+          },
+          {
+            path: "/admin/dashboard/orders",
+            element: <AdminOrders />,
+          },
+        ],
+      },
       { path: "brands", element: <Brands /> },
       { path: "brands/:vendor", element: <Brand /> },
-      { path: "auth", element: <Authentication /> },
+      { path: "auth", element: <AuthenticationPage /> },
+      {
+        path: "/admin/sign-in",
+        element: <AdminAuthenticationPage />,
+      },
       { path: "checkout", element: <CheckOutPage /> },
       {
         path: "checkout/payment",
-        element: (
-            <PaymentPage />
-        ),
+        element: <PaymentPage />,
       },
       { path: "not-found", element: <NotFoundPage /> },
       { path: "*", element: <Navigate replace to="/not-found" /> },
