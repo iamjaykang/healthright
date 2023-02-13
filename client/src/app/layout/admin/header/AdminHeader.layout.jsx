332fada2cfa2ce621a6762dcca4ed46366f26116
Jay Kang <jaykang0395@gmail.com>
2023-02-14 02:08:28 +1300

    Added admin layouts

---
+++ client/src/app/layout/admin/header/AdminHeader.layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AdminHeader = () => {
+  return (
+    <header className="dashboard-header">
+    <div className="dashboard-title">Healthright Dashboard</div>
+    <div className="dashboard-user-info">
+      <div className="dashboard-user-name">Jay Kang</div>
+      <div className="dashboard-user-dropdown">
+        <ul className="dashboard-user-dropdown-list">
+          <li className="dashboard-user-dropdown-list-item">Profile</li>
+          <li className="dashboard-user-dropdown-list-item">Settings</li>
+          <li className="dashboard-user-dropdown-list-item">Sign Out</li>
+        </ul>
+      </div>
+    </div>
+  </header>
+  )
+}
+
+export default AdminHeader
