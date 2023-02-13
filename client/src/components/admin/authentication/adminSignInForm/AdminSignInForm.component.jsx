c56ace0dad5e798e4ecf714eca9626347c4a38e4
Jay Kang <jaykang0395@gmail.com>
2023-02-14 02:07:16 +1300

    Added admin components for sign-in page and dashboard

---
+++ client/src/components/admin/authentication/adminSignInForm/AdminSignInForm.component.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Button from "../../../../app/common/button/Button.common";
+import MyTextInput from "../../../../app/common/form/MyTextInput.common";
+import { adminEmailSignInLoading } from "../../../../app/stores/user/user.action";
+import {
+  selectCurrentUser,
+} from "../../../../app/stores/user/user.selector";
+
+const initialFormValues = {
+  email: "",
+  password: "",
+};
+
+const AdminSignInForm = () => {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const { email, password } = formValues;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const currentUser = useSelector(selectCurrentUser);
+
+  const resetFormValues = () => {
+    setFormValues(initialFormValues);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      dispatch(adminEmailSignInLoading(email, password));
+      resetFormValues();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    if (currentUser) {
+      navigate("/admin/dashboard");
+    }
+  }, [currentUser, navigate]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <MyTextInput
+        label="Email"
+        type="email"
+        required
+        onChange={handleChange}
+        name="email"
+        value={email}
+      />
+      <MyTextInput
+        label="Password"
+        type="password"
+        required
+        onChange={handleChange}
+        name="password"
+        value={password}
+      />
+      <div className="btns-container">
+        <Button type="submit">Sign In</Button>
+      </div>
+    </form>
+  );
+};
+
+export default AdminSignInForm;
---
+++ client/src/components/admin/dashboard/home/AdminHome.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./AdminHome.css";
+
+const AdminHome = () => {
+  return <div>Home</div>;
+};
+
+export default AdminHome;
