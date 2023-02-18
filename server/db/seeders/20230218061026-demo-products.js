394c5b2ea7c575f0269e9fbd7ed7704a19fb1280
Jay Kang <jaykang0395@gmail.com>
2023-02-18 19:25:56 +1300

    Updated seed file

--- server/db/seeders/20230218061026-demo-products.js
+++ server/db/seeders/20230218061026-demo-products.js
@@ -5,186 +5,287 @@ const productService = require("../../services/product.service");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const productData = [
+    await queryInterface.bulkInsert("productCategories", [
       {
+        categoryName: "Colostrum",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        categoryName: "Propolis",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        categoryName: "Collagen",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        categoryName: "Multi Vitamin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        categoryName: "Green Mussels",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        categoryName: "Joint Care",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        categoryName: "Eye Care",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        categoryName: "Immune Support",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        categoryName: "Cissus",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+
+    await queryInterface.bulkInsert("productVendors", [
+      {
+        vendorName: "PaiOra",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        vendorName: "kuku",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        vendorName: "Nu.wise",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        vendorName: "Nzpurehealth",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+
+    await queryInterface.bulkInsert("products", [
+      {
+        categoryId: 2,
+        vendorId: 4,
         name: "Nzpurehealth Propolis 100 capsules",
         description: "<p>Nzpurehealth Propolis 100 capsules</p>",
         productImage:
           "https://i.ibb.co/hx0kczV/N-680001-nzpurehealth-propolis-100c.jpg",
         price: 25,
         qtyInStock: 30,
-        categoryName: "Propolis",
-        vendorName: "Nzpurehealth",
         cost: 10,
-        productLive: 0
+        productLive: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        categoryId: 1,
+        vendorId: 4,
         name: "Nzpurehealth Bovine Colostrum 100 capsules",
         description: "<p>Nzpurehealth Bovine Colostrum 100 capsules</p>",
         productImage:
           "https://i.ibb.co/rvZ4b5S/N-680003-NP-Bovine-Colostrum-100c.jpg",
         price: 25,
         qtyInStock: 30,
-        categoryName: "Colostrum",
-        vendorName: "Nzpurehealth",
         cost: 10,
-        productLive: 0
+        productLive: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        categoryId: 3,
+        vendorId: 4,
         name: "Nzpurehealth Marine Collagen 60 capsules",
         description: "<p>Nzpurehealth Marine Collagen 60 capsules</p>",
         productImage:
           "https://i.ibb.co/3RnqzkJ/N-680004-Marine-collagen-60c.jpg",
         price: 25,
         qtyInStock: 30,
-        categoryName: "Collagen",
-        vendorName: "Nzpurehealth",
         cost: 10,
-        productLive: 0
+        productLive: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        categoryId: 4,
+        vendorId: 4,
         name: "Nzpurehealth Multivitamin Plus 1500mg 60 capsules",
         description: "<p>Nzpurehealth Multivitamin Plus 1500mg 60 capsules</p>",
         productImage:
           "https://i.ibb.co/6PRxCX6/N-680005-Multivitamin-Plus-1500mg-60c.jpg",
         price: 25,
         qtyInStock: 30,
-        categoryName: "Multi Vitamin",
-        vendorName: "Nzpurehealth",
         cost: 10,
-        productLive: 0
+        productLive: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        categoryId: 5,
+        vendorId: 4,
         name: "Nzpurehealth Pure Pets Green Lipped Mussel 120 capsules",
         description: "<p>Nzpurehealth Pure Pets Green Lipped Mussel 120 capsules</p>",
         productImage:
           "https://i.ibb.co/4ZLZD7P/N-680006-NP-Pure-pets-green-lipped-mussel-120c.jpg",
         price: 25,
         qtyInStock: 30,
-        categoryName: "Green Mussels",
-        vendorName: "Nzpurehealth",
         cost: 10,
-        productLive: 0
+        productLive: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 2,
+        categoryId: 5,
         price: 25,
-        vendorName: "kuku",
-        categoryName: "Green Mussels",
         qtyInStock: 50,
         name: "kuku GLM 12500 45c",
         description: "<p>kuku GLM 12500 45c</p>",
         productImage:
           "https://i.ibb.co/4fd2frP/Kuku-GLM-Oil-Range-Blister-12500-Maintenance-45-Caps-5.jpg",
           cost: 10,
-          productLive: 0
+          productLive: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 2,
+        categoryId: 5,
         price: 25,
         productImage: "https://i.ibb.co/t85xhDb/glm-20250.jpg",
         name: "kuku GLM 20250",
         description: "<p>kuku GLM 20250</p>",
-        vendorName: "kuku",
-        categoryName: "Green Mussels",
         qtyInStock: 50,
         cost: 10,
-        productLive: 0
+        productLive: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
-        vendorName: "kuku",
-        categoryName: "Green Mussels",
+        vendorId: 2,
+        categoryId: 5,
         qtyInStock: 50,
         price: 25,
         name: "kuku GLM27000 45c",
         description: "<p>kuku GLM27000 45c</p>",
         productImage: "https://i.ibb.co/x5y4nV5/kuku-glm27000-45.jpg",
         cost: 10,
-        productLive: 0
+        productLive: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 2,
+        categoryId: 5,
         price: 25,
         productImage:
           "https://i.ibb.co/r0KkWHT/KUKU-Mussel-Oil-37000-45c-4.jpg",
-        vendorName: "kuku",
-        categoryName: "Green Mussels",
         qtyInStock: 50,
         name: "kuku GLM37000 45c",
         description: "<p>kuku GLM37000 45c</p>",
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 2,
+        categoryId: 5,
         name: "kuku GLM37000 combo",
         description: "<p>kuku GLM37000 combo</p>",
         price: 25,
-        vendorName: "kuku",
-        categoryName: "Green Mussels",
         qtyInStock: 50,
         productImage:
           "https://i.ibb.co/QNwXhxL/Kuku-Box-and-Bottle-with-caps-and-blister-card-1000x1000px.jpg",
           cost: 8,
-          productLive: 1
+          productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 2,
+        categoryId: 5,
         name: "kuku pet GLM",
         description: "<p>kuku pet GLM</p>",
         productImage:
           "https://i.ibb.co/KwjYXrq/Kuku-Pet-Health-Care-150ml-White-Bottle-v4-x1000.jpg",
-        vendorName: "kuku",
-        categoryName: "Green Mussels",
         qtyInStock: 50,
         price: 25,
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 3,
+        categoryId: 6,
         productImage:
           "https://i.ibb.co/PM8hmG8/N-650003-Joint-plus-11000mg-90c-1.jpg",
         price: 25,
         description: "<p>Nu.wise Nu.Joint</p>",
         name: "Nu.wise Nu.Joint",
-        vendorName: "Nu.wise",
-        categoryName: "Joint Care",
         qtyInStock: 50,
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
-        vendorName: "Nu.wise",
-        categoryName: "Eye Care",
+        vendorId: 3,
+        categoryId: 7,
         qtyInStock: 50,
         name: "Nu.wise Nu.Eyes",
         description: "<p>Nu.wise Nu.Eyes</p>",
         price: 25,
         productImage: "https://i.ibb.co/hZjLcs8/N-650006-Nu-colo-300-2-2.jpg",
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 3,
+        categoryId: 8,
         price: 25,
         name: "Nu.wise Nu.Colostrum",
         description: "<p>Nu.wise Nu.Colostrum</p>",
-        vendorName: "Nu.wise",
-        categoryName: "Immune Support",
         qtyInStock: 50,
         productImage: "https://i.ibb.co/Fb4VXNy/N-650006-Nu-colo-300-5.jpg",
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 3,
+        categoryId: 2,
         name: "Nu.wise Nu.Immunex Propolis",
         description: "<p>Nu.wise Nu.Immunex Propolis</p>",
-        vendorName: "Nu.wise",
-        categoryName: "Propolis",
         qtyInStock: 50,
         productImage:
           "https://i.ibb.co/vPbvKLZ/N-650018-Nu-Wise-Nu-Immunex-Propolis-120caps.jpg",
         price: 25,
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
-        vendorName: "Nu.wise",
-        categoryName: "Cissus",
+        vendorId: 3,
+        categoryId: 9,
         qtyInStock: 50,
         price: 25,
         productImage:
@@ -192,35 +293,41 @@ module.exports = {
         name: "Nu.wise Nu.Slim Cissus",
         description: "<p>Nu.wise Nu.Slim Cissus</p>",
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 1,
+        categoryId: 8,
         productImage:
           "https://i.ibb.co/bz3JVPV/P-100001-detail-piora-colo-plus-300t-1.jpg",
-        vendorName: "PaiOra",
-        categoryName: "Immune Support",
         qtyInStock: 50,
         price: 25,
         name: "PaiOra Colo Plus Colostrum 300 tablets",
         description: "<p>PaiOra Colo Plus Colostrum 300 tablets</p>",
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 1,
+        categoryId: 8,
         name: "PaiOra Colo Deer Placenta 2500 120 capsules",
         description: "<p>PaiOra Colo Deer Placenta 2500 120 capsules</p>",
-        vendorName: "PaiOra",
-        categoryName: "Immune Support",
         qtyInStock: 50,
         productImage:
           "https://i.ibb.co/fdswLnF/P-100004-Pai-Ora-Deer-Placenta-2500-120c-v2-1.jpg",
         price: 25,
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
-        vendorName: "PaiOra",
-        categoryName: "Propolis",
+        vendorId: 1,
+        categoryId: 2,
         qtyInStock: 50,
         productImage:
           "https://i.ibb.co/k1q0THk/P-100009-Pai-Ora-Propolis-60s-1.jpg",
@@ -228,46 +335,54 @@ module.exports = {
         name: "PaiOra Propolis 7000 60 capsules",
         description: "<p>PaiOra Propolis 7000 60 capsules</p>",
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 1,
+        categoryId: 2,
         price: 25,
         name: "PaiOra Propolis 7000 120 capsules",
         description: "<p>PaiOra Propolis 7000 120 capsules</p>",
-        vendorName: "PaiOra",
-        categoryName: "Propolis",
         qtyInStock: 50,
         productImage:
           "https://i.ibb.co/p1QxCJ0/P-1000010-Pai-Ora-Propolis-120s-1.jpg",
           cost: 8,
-          productLive: 1
+          productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 1,
+        categoryId: 9,
         productImage: "https://i.ibb.co/9tgmcDH/Pai-Ora-Cissus-90tab.jpg",
-        vendorName: "PaiOra",
-        categoryName: "Cissus",
         qtyInStock: 50,
         name: "PaiOra Cissus 90 tablets",
         description: "<p>PaiOra Cissus 90 tablets</p>",
         price: 25,
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
+        vendorId: 1,
+        categoryId: 6,
         productImage:
           "https://i.ibb.co/Mpjvwh5/Paiora-GLM6500-Condroitin-300c-1.jpg",
         price: 25,
-        vendorName: "PaiOra",
-        categoryName: "Joint Support",
         qtyInStock: 50,
         name: "PaiOra GLM 6500 Condroitin 300 capsules",
         description: "<p>PaiOra GLM 6500 Condroitin 300 capsules</p>",
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
-        vendorName: "PaiOra",
-        categoryName: "Immune Support",
+        vendorId: 1,
+        categoryId: 8,
         qtyInStock: 50,
         name: "PaiOra Goat Colo Protein Powder 250g",
         description: "<p>PaiOra Goat Colo Protein Powder 250g</p>",
@@ -275,13 +390,11 @@ module.exports = {
           "https://i.ibb.co/F3zn5kL/Premiun-Goat-Colo-protein-Powder-250g-1.jpg",
         price: 25,
         cost: 8,
-        productLive: 1
+        productLive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
-    ];
-
-    for (const data of productData) {
-      await productService.createProduct(data);
-    }
+    ]);
   },
 
   async down(queryInterface) {
