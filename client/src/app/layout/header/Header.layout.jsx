e8934ed67265c432b22e6ddb29a75fb976c5dec4
Jay Kang <jaykang0395@gmail.com>
2023-02-22 18:50:54 +1300

    Added header logo and changed styling

--- client/src/app/layout/header/Header.layout.jsx
+++ client/src/app/layout/header/Header.layout.jsx
@@ -6,6 +6,7 @@ import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../../app/stores/user/user.selector";
 import { selectIsCartOpen } from "../../../app/stores/cart/cart.selector";
 import MobileNav from "./mobileNav/MobileNav.layout";
+import { ImLeaf } from "react-icons/im";
 
 const Header = () => {
   const currentUser = useSelector(selectCurrentUser);
@@ -16,7 +17,10 @@ const Header = () => {
       <header className="app__header shadow-sm">
         <div className="app__header-content-container">
           <Link className="app__header-brand" to="/">
-            <span className="app__header-logo">Healthright</span>
+            <span className="app__header--brand-title">Healthright</span>
+            <span className="app__header--logo">
+              <ImLeaf className="leaf-icon"/>
+            </span>
           </Link>
           <div className="app__header-nav-container">
             <Navbar currentUser={currentUser} />
